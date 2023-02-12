202a06e7d123ce07903464d27cb113b6fb71ac3e
KAWSAR KABIR <kawsarkabir11@gmail.com>
2023-02-12 15:57:11 +0600

    sum of number using function

---
+++ Day-05/sumOfNumber.js
@@ -0,0 +1,13 @@
+// sum of number 
+function sumOfNumber(number){
+  let sum = 0;
+  for (let i = 0; i <= 10; i++){
+    sum = sum + i;
+    console.log(sum);
+  }
+  return sum;
+}
+sumOfNumber();
+
+
+
